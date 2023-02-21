94202027545cfdaf8726a8d35834177340a60f38
daniisza <danidmta@gmail.com>
2023-02-20 20:03:32 -0500

    On Start
    aqui se hace la depuracion con los on

--- CLASE 20 FEBRERO/app/src/main/java/com/daniela/clasefebrero20/MainActivity.java
+++ CLASE 20 FEBRERO/app/src/main/java/com/daniela/clasefebrero20/MainActivity.java
@@ -22,5 +22,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        Log.i(TAG, "Entré a OnStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "Entré a OnRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "Entré a OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "Entré a OnPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "Entré a OnStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "Entré a OnDestroy");
     }
 }
