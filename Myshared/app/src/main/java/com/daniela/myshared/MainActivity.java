fd298ace08ce38352e2cb0726dedb5020343f9d7
daniisza <danidmta@gmail.com>
2023-04-10 19:42:56 -0500

    GUARDAR
    Se llama el metodo guardar

--- Myshared/app/src/main/java/com/daniela/myshared/MainActivity.java
+++ Myshared/app/src/main/java/com/daniela/myshared/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,5 +33,17 @@ public class MainActivity extends AppCompatActivity {
         Obj_editor.putString("usuarios", etyusuario.getText().toString());
         Obj_editor.commit();
         finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                guardar(view);
+            }
+        });
     }
 }
